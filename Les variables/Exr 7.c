538ffc1f86fcbc68682409789c6a1ca8d3702c1b
yassmina16 <belghiti.y1617@gmail.com>
2025-09-03 10:26:00 +0200

    Update Exr 7.c

--- Les variables/Exr 7.c
+++ Les variables/Exr 7.c
@@ -7,10 +7,10 @@ void calculerMoyenne() {
     printf("Entrez 1er nombre");
       scanf("%f", &num1);
 
-    printf("Entrez 2er nombre");
+    printf("Entrez 2ème nombre");
       scanf("%f", &num2);
 
-    printf("Entrez 1er nombre");
+    printf("Entrez 3ème nombre");
       scanf("%f", &num3);
 
     resultat = (num1 * 2 + num2 * 3 + num3 * 5) / 10.0;
