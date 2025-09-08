0bef650ad9cfde760880561567c8fe18fe23a5ae
yassmina <belghiti.y1617@gmail.com>
2025-09-08 19:03:21 +0200

    Exr 8

---
+++ Day 4/les Tableaux/Exr 7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void OrdreArray() {
+int n ;
+  printf("Entrez le nembre (maximum 100) : "); 
+     scanf("%d", &n);
+   int arr[n];  
+    for (int i = 0; i < n; i++) { 
+      printf("Entrez les éléments:%d", i + 1); 
+       scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {   
+        for(int j = 0 ; j < n - 1 ; j++){ 
+            if (arr[j+1] > arr[j]) {
+                int temp = arr[j]; 
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+
+    }
+    for(int i = 0 ; i < n ; i++){
+        printf("%d\n" , arr[i]);
+    }
+}
+
+int main() {
+OrdreArray();  
+return 0;
+}
---
+++ Day 4/les Tableaux/Exr 8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void  CopieArray() {
+int n ;
+   printf("Entrez la taille du tableaux : \n"); 
+     scanf("%d", &n);
+   int arr[n];  
+    for (int i = 0; i < n; i++) { 
+      printf("Entrez les elements %d : ", i + 1); 
+       scanf("%d", &arr[i]);
+    }
+    int b[n];
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n") ;
+    for (int i = 0 ; i < n ; i++) { 
+        b[i] = arr[i]; 
+        printf("%d ", b[i]); 
+    }
+
+}
+
+int main() { 
+CopieArray();  
+return 0;
+}
