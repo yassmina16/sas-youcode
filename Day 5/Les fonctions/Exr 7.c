f6c3fb9ca56db8cb6dbc0af1c4d4b94fedad1c1a
yassmina <belghiti.y1617@gmail.com>
2025-09-09 15:12:42 +0200

    Exr 7

---
+++ Day 5/Les fonctions/Exr 7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void inverser() {
+    char str[100];
+    printf("Entrez la chaîne : ");
+    scanf("%s", str);
+    int start = 0;
+    int end = 0;
+    while (str[end] != '\0') {
+        end++;
+    }
+    end--;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+    printf("%s\n", str);
+}
+
+int main() {
+inverser();
+return 0;
+}
