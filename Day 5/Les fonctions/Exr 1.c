be74b89865a9ccb10ff174d5c09a08b1978220c9
yassmina <belghiti.y1617@gmail.com>
2025-09-09 10:22:35 +0200

    Exr 1

---
+++ Day 5/Les fonctions/Exr 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void somme() {
+int a , b ;
+    printf("Entrez un nembre:");
+      scanf("%d", &a);
+    printf("Entrez un nembre:");
+      scanf("%d", &b);
+    printf(" %d\n", a + b);
+}
+
+int main() {
+somme();
+return 0;
+}
