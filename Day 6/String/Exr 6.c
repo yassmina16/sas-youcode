fc9f3ba313b6a0938856a058b0531be5b1b8ca3d
yassmina <belghiti.y1617@gmail.com>
2025-09-09 18:10:33 +0200

    Exr 7

--- Day 6/String/Exr 6.c
+++ Day 6/String/Exr 6.c
@@ -4,17 +4,17 @@
 void count() {
 char str[100]; 
 char c ;
-int counter = 0;
+int Result = 0;
     printf("Entrez la chaîne : ");
     scanf("%s", &str);
     printf("Entrez la chaîne : ");
     scanf(" %c", &c);
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == c) {
-        counter++;
+        Result++;
         }
     }
-    printf(" %d", counter);
+    printf(" %d", Result);
 }
 
 int main() {
