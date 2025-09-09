8dc56be55ba476fb56c1a64b4008662ab09c9b72
yassmina <belghiti.y1617@gmail.com>
2025-09-09 10:33:19 +0200

    Exr 2

---
+++ Day 5/Les fonctions/Exr 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void Mult() {
+int a , b ;
+    printf("Entrez un nembre:");
+      scanf("%d", &a);
+    printf("Entrez un nembre:");
+      scanf("%d", &b);
+    printf(" %d\n", a * b);
+}
+
+int main() {
+Mult();
+return 0;
+}
