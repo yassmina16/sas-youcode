7bb0dd5e248a312cf6ec49b7b478d4e3750aeb6e
yassmina <belghiti.y1617@gmail.com>
2025-09-11 12:04:01 +0200

    Exr 2

--- Day 7/les Structures/Exr 2.c
+++ Day 7/les Structures/Exr 2.c
@@ -3,19 +3,25 @@
 struct person {
     char nom[50];
     char prenom[50];
-    float note;
+    float note1[4];
+
 };
 
 void affi() {
     struct person p ;
     printf("Entrez votre nom :" );
-    scanf("%s",&p.nom);
+    scanf("%s" , &p.nom);
     printf("Entrez votre prenom :"); 
-    scanf("%s",&p.prenom);
-    printf("Entrez votre note :" ); 
-    scanf("%f", &p.note);
-      printf("informations personnelles :");
-      printf("\n Nom: %s\n  Prenom: %s\n  note: %.2f\n", p.nom, p.prenom,p.note);
+    scanf("%s" , &p.prenom);
+    for(int i = 0 ; i < 4 ; i++) {
+        printf("Entrez votre note1 :%d", i + 1 ); 
+        scanf("%f", &p.note1[i]);
+    }
+    printf("informations personnelles :");
+    printf("\n Nom: %s\n Prenom: %s\n " , p.nom, p.prenom );
+    for(int i = 0 ; i < 4 ; i++){
+        printf("%.2f\n" , p.note1[i]);
+    }
 }
 
 int main() {
