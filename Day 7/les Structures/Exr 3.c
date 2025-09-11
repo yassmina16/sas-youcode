03a4e8091c02b64d2894bbff65fb2a0f7dd414ee
yassmina <belghiti.y1617@gmail.com>
2025-09-11 11:03:48 +0200

    Exr 3

---
+++ Day 7/les Structures/Exr 3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct Rect {
+    int r, g;
+};
+
+void area() {
+    struct Rect R ;
+    printf("Entrez largeur :" );
+        scanf("%d",&R.r);
+    printf("Entrez longeur :" );
+        scanf("%d",&R.g);
+    printf("Area: %d\n", R.r * R.g);
+}
+
+int main() {
+area();
+return 0;
+}
