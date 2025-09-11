a385a857a088005b067f5cf111f48922ab784a16
yassmina <belghiti.y1617@gmail.com>
2025-09-11 10:20:51 +0200

    Exr 1

--- Day 7/les Structures/Exr 1.c
+++ Day 7/les Structures/Exr 1.c
@@ -2,27 +2,21 @@
 #include <string.h>
 
 struct person {
-    char name[50];
-    char fname[50];
+    char nom[50];
+    char prenom[50];
     int age;
 };
 
 void aff() {
     struct person p ;
-    char nom[50];
-    char prenom[50];
-    int age;
-    printf("Entrez votre nom" );
-    scanf("%s",&nom);
-    strcpy(p.name , nom);
-    printf("Entrez votre prénom"); 
-    scanf("%s",&prenom);
-    strcpy(p.fname , prenom);
-    printf("Entrez votre age" ); 
-    scanf("%d", &age);
-    p.age = age;
-      printf("informations personnelles");
-      printf("Nom: %s, Prenom: %s, Age: %d\n", p.name, p.fname,p.age);
+    printf("Entrez votre nom :" );
+    scanf("%s",&p.nom);
+    printf("Entrez votre prenom :"); 
+    scanf("%s",&p.prenom);
+    printf("Entrez votre age :" ); 
+    scanf("%d", &p.age);
+      printf("informations personnelles :");
+      printf("Nom: %s, Prenom: %s, Age: %d\n", p.nom, p.prenom,p.age);
 }
 
 int main() {
