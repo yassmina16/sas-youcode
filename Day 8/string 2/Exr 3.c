d02023c9452974f9bd32b6caf0c888894693087c
yassmina <belghiti.y1617@gmail.com>
2025-09-12 17:26:11 +0200

    Exr 3

---
+++ Day 8/string 2/Exr 3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+void count() {
+char str[100];
+int count = 0;
+    printf("Entrez la chaîne : ");
+    fgets(str , sizeof(str) , stdin);
+     char *word = strtok(str, " " );
+    while(word != NULL){
+        count ++ ;
+        word = strtok(NULL , " ");
+    }
+    printf("%d" , count);
+}
+
+int main(){
+    count();
+    return 0;
+}
+
+
+
